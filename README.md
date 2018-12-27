b28356f8525c90aab8d9b87044439a279c682df8
gloria <pureainu@gmail.com>
2018-12-27 11:31:29 +0900

    [Add] express middleware

--- README.md
+++ README.md
@@ -26,9 +26,35 @@ babel-node index.js
 - 코드가 변경될 때마다 수동으로 재기동하지 않고, 자동으로 변경된 내용이 반영되어 재기동되도록 해주는 기능을 제공
 - 설치
 ```bash
-yarn add nodemon --dev
+yarn add nodemon
 ```
 - 실행 방법
 ```bash
 nodemon --exec babel-node index.js
 ```
+- 파일 수정 후 일정 시간 후에 Restart가 되도록 하려면 `--delay` 옵션을 추가한다.
+```bash
+nodemon --exec babel-node index.js --delay 10
+```
+
+#### express middleware
+###### morgan
+- https://github.com/expressjs/morgan
+- logging을 위한 미들웨어
+```javascript
+import morgan from 'morgan';
+
+// logging format을 파라미터로 전달
+app.use(morgan('tiny'));
+```
+
+###### helmet
+- https://helmetjs.github.io
+- HTTP 헤더를 적절히 설정하여 몇 가지 잘 알려진 웹 취약성으로부터 앱을 보호할 수 있다.
+```javascript
+import helmet from 'helmet';
+
+app.use(helmet());
+```
+
+###### cookie
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -6,15 +9,30 @@ const HOST = 'localhost';
 const PORT = 3000;
 
 /**
+ * middleware
+ */
+app.use(morgan('tiny'));
+app.use(helmet());
+// Express v4.16.0 기준으로는 body-parser를 별도로 설치하지 않고 아래와 같이 설정해도 됨
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+/**
  * define router
  */
 app.get('/', function (req, res) {
+  console.log(req.body);
+  console.log('Cookies: ', req.cookies)
+  console.log('Signed Cookies: ', req.signedCookies)
   res.send('This is Index Page !!');
 });
 
 app.get('/profile', function (req, res) {
   res.send('This is Profile Page');
 })
+
 
 /**
  * set port
